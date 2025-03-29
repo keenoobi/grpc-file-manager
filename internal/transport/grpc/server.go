d68d314b8306f24d5441ae5f9c9f149e6e6c30dc
keenoobi <koksharovanwork@gmail.com>
2025-03-29 16:20:30 +0300

    implement gRPC transport

---
+++ internal/transport/grpc/server.go
@@ -0,0 +1,134 @@
+package grpc
+
+import (
+	"context"
+	"io"
+
+	"github.com/keenoobi/grpc-file-manager/api/proto"
+	"github.com/keenoobi/grpc-file-manager/internal/usecase"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type fileServiceServer struct {
+	proto.UnimplementedFileServiceServer
+	fileUseCase usecase.FileUseCase
+}
+
+func NewFileServiceServer(fileUseCase usecase.FileUseCase) proto.FileServiceServer {
+	return &fileServiceServer{fileUseCase: fileUseCase}
+}
+
+func (s *fileServiceServer) UploadFile(stream proto.FileService_UploadFileServer) error {
+	req, err := stream.Recv()
+	if err != nil {
+		return status.Errorf(codes.Unknown, "cannot receive file info")
+	}
+
+	metadata := req.GetMetadata()
+	if metadata == nil {
+		return status.Errorf(codes.InvalidArgument, "first message must contain metadata")
+	}
+
+	filename := metadata.GetFilename()
+	if filename == "" {
+		return status.Errorf(codes.InvalidArgument, "filename is required")
+	}
+
+	pr, pw := io.Pipe()
+	defer pr.Close()
+
+	go func() {
+		defer pw.Close()
+		for {
+			req, err := stream.Recv()
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				pw.CloseWithError(err)
+				return
+			}
+
+			chunk := req.GetChunk()
+			if _, err := pw.Write(chunk); err != nil {
+				pw.CloseWithError(err)
+				return
+			}
+		}
+	}()
+
+	file, err := s.fileUseCase.UploadFile(stream.Context(), filename, pr)
+	if err != nil {
+		return status.Errorf(codes.Internal, "cannot save file: %v", err)
+	}
+
+	return stream.SendAndClose(&proto.UploadFileResponse{
+		Filename:  file.Name,
+		Size:      uint64(file.Size),
+		CreatedAt: timestamppb.New(file.CreatedAt),
+	})
+}
+
+func (s *fileServiceServer) DownloadFile(req *proto.DownloadFileRequest, stream proto.FileService_DownloadFileServer) error {
+	file, reader, err := s.fileUseCase.DownloadFile(stream.Context(), req.GetFilename())
+	if err != nil {
+		return status.Errorf(codes.Internal, "cannot read file: %v", err)
+	}
+	defer reader.Close()
+
+	// Отправляем метаданные первым сообщением
+	if err := stream.Send(&proto.DownloadFileResponse{
+		Content: &proto.DownloadFileResponse_Metadata{
+			Metadata: &proto.FileMetadata{
+				Filename:  file.Name,
+				Size:      uint64(file.Size),
+				CreatedAt: timestamppb.New(file.CreatedAt),
+			},
+		},
+	}); err != nil {
+		return status.Errorf(codes.Internal, "cannot send metadata: %v", err)
+	}
+
+	buffer := make([]byte, 1024)
+	for {
+		n, err := reader.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return status.Errorf(codes.Internal, "cannot read chunk: %v", err)
+		}
+
+		if err := stream.Send(&proto.DownloadFileResponse{
+			Content: &proto.DownloadFileResponse_Chunk{
+				Chunk: buffer[:n],
+			},
+		}); err != nil {
+			return status.Errorf(codes.Internal, "cannot send chunk: %v", err)
+		}
+	}
+	return nil
+}
+
+func (s *fileServiceServer) ListFiles(ctx context.Context, req *proto.ListFilesRequest) (*proto.ListFilesResponse, error) {
+	files, err := s.fileUseCase.ListFiles(ctx)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "cannot list files: %v", err)
+	}
+
+	response := &proto.ListFilesResponse{
+		Files: make([]*proto.FileInfo, len(files)),
+	}
+
+	for i, file := range files {
+		response.Files[i] = &proto.FileInfo{
+			Filename:  file.Name,
+			CreatedAt: timestamppb.New(file.CreatedAt),
+			UpdatedAt: timestamppb.New(file.UpdatedAt),
+		}
+	}
+
+	return response, nil
+}
